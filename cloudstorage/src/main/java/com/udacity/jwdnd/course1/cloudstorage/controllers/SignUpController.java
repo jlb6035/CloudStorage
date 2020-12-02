c83afa045d7dd74e1c56c64c3dd2e12d4313e8cc
jlb6035 <jlb6035@gmail.com>
2020-12-02 14:10:54 -0500

    addressed comments in review

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignUpController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignUpController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller()
 @RequestMapping("/signup")
@@ -27,7 +28,7 @@ public class SignUpController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
 
         if (!userService.isUserNameAvailable(user.getUsername())) {
@@ -43,6 +44,8 @@ public class SignUpController {
 
         if (signupError == null) {
             model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("SuccessMessage","Sign Up Successfully");
+            return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
         }
